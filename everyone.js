c457753447373c6cf829d1627d6b17e5ffe6c81b
Steven <steven@ceriously.com>
2018-09-21 20:41:46 -0400

    Add everyone page

--- everyone.js
+++ everyone.js
@@ -1,7 +1,27 @@
-var container = document.querySelector('.container');
+var content = document.querySelector('#content');
+var fallbackUrl = 'https://res.cloudinary.com/ceriously/image/upload/avatar_ax5bsq.png';
 
-container.innerHTML = backers.map(function(hash) {
-    var url = 'https://res.cloudinary.com/ceriously/image/upload/avatar_ax5bsq.png';
-    //var url = getUrl(hash);
-    return '<img class="rounded-circle" width="64" height="64" src="'+url+'" />';
-}).join('\n');
+function newImage() {
+    var img = new Image();
+    img.className = 'rounded-circle';
+    img.width = '64';
+    img.height = '64';
+    return img;
+}
+
+backers.forEach(function(hash) {
+    var img = newImage();
+    img.onload = function() {
+        content.appendChild(img);
+    };
+    img.onerror = function() {
+        /*
+        var fallback = newImage();
+        fallback.src = fallbackUrl;
+        content.appendChild(fallback);
+        */
+    };
+    img.src = getUrl(hash);
+})
+
+content.innerHTML = html;
